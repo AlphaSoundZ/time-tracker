342a44c22243d53438039e544e4dd1b2e3fa5867
AlphaSoundZ <jan.jacob.holst@gmail.com>
2023-04-08 00:06:41 +0200

    ✨ feat: Login and Dashboard

---
+++ time-tracker/src/views/DashboardView.vue
@@ -0,0 +1,110 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+//import { createClient } from "@supabase/supabase-js";
+import { supabase } from '../lib/supabaseClient';
+
+const trackings = ref([])
+
+async function getTrackings() {
+  const { data } = await supabase.from('trackings').select();
+  trackings.value = data;
+}
+
+function duration(tracking) {
+  // if tracking has not ended, return "in progress"
+  if (!tracking.end) {
+    return "in progress";
+  }
+
+  // calculate duration
+  const start = new Date(tracking.start);
+  const end = new Date(tracking.end);
+  const duration = end - start;
+
+  // convert duration to hours, minutes, seconds
+  const hours = Math.floor(duration / 1000 / 60 / 60);
+  const minutes = Math.floor(duration / 1000 / 60) % 60;
+  const seconds = Math.floor(duration / 1000) % 60;
+
+  // format duration
+  const formattedDuration = [hours, minutes, seconds]
+    .map((unit) => String(unit).padStart(2, '0'))
+    .join(':');
+
+  return formattedDuration;
+}
+
+function formatTime(time) {
+  if (!time) return;
+  
+  // retrun time only
+  return new Date(time).toLocaleTimeString().split(' ')[0];
+}
+
+function getDate(tracking) {
+  let startDate = new Date(tracking.start).toLocaleDateString();
+
+  if (tracking.end) {
+    let endDate = new Date(tracking.end).toLocaleDateString();
+
+    if (startDate === endDate) {
+      return startDate;
+    } else {
+      return `${startDate} - ${endDate}`;
+    }
+  }
+
+  return startDate;
+}
+
+onMounted(() => {
+  getTrackings();
+})
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="title">Time Tracker</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>User</th>
+          <th>Date</th>
+          <th>Start</th>
+          <th>End</th>
+          <th>Duration</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="tracking in trackings" :key="tracking.id">
+          <td>{{ tracking.user_id }}</td>
+          <td>{{ getDate(tracking) }}</td>
+          <td>{{ formatTime(tracking.start) }}</td>
+          <td>{{ formatTime(tracking.end) }}</td>
+          <td>{{ duration(tracking) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.table th, .table td {
+  border: 1px solid #3e3e3e;
+  padding: 0.5rem;
+}
+
+.table th {
+  background: #0a0a0a;
+}
+</style>
---
+++ time-tracker/src/views/HomeView.vue
@@ -0,0 +1,87 @@
+<template>
+  <main>
+    <h1>Welcome {{user_name}}!</h1>
+    <a href="#" id="loginBtn" @click="signIn">Google Sign-In</a>
+    <a href="#" id="logoutBtn">Logout</a>
+  </main>
+</template>
+<script>
+import { ref, onMounted } from 'vue';
+import { supabase } from '../lib/supabaseClient';
+
+export default {
+  data() {
+    return {
+      user_name: '',
+    }
+  },
+  methods: {
+    async signIn() {
+      supabase.auth.signInWithOAuth({
+        provider: 'google',
+      });
+
+      this.updateUI();
+    },
+    async signOut() {
+      await supabase.auth.signOut();
+      
+      console.log('signing out');
+      this.updateUI();
+    },
+    async updateUI() {
+      const signinButton = document.getElementById('loginBtn');
+      const logoutButton = document.getElementById('logoutBtn');
+      
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user) {
+        this.user_name = user.user_metadata.full_name;
+
+        signinButton.style.display = 'none';
+        logoutButton.style.display = 'block';
+      }
+      else {
+        this.user_name = 'Guest';
+
+        signinButton.style.display = 'block';
+        logoutButton.style.display = 'none';
+      }
+    }
+  },
+  async mounted() {
+    const signinButton = document.getElementById('loginBtn');
+    const logoutButton = document.getElementById('logoutBtn');
+
+    logoutButton.addEventListener('click', this.signOut);
+    signinButton.addEventListener('click', this.signIn);
+    
+    this.updateUI();
+  }
+}
+</script>
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
